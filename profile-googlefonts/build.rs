5d65344a79f5984c2880399ca0aaa5c0cdb13714
Simon Cozens <simon@simon-cozens.org>
2024-07-10 22:11:38 +0100

    Tidy up build script

--- profile-googlefonts/build.rs
+++ profile-googlefonts/build.rs
@@ -1,12 +1,8 @@
 fn main() {
-    println!("Building protos");
     protobuf_codegen::Codegen::new()
         .pure()
-        // All inputs and imports from the inputs must reside in `includes` directories.
         .includes(&["src/protos"])
         .input("src/protos/fonts_public.proto")
-        // Specify output directory relative to Cargo output directory.
         .cargo_out_dir("protos")
         .run_from_script();
-    println!("cargo::rerun-if-changed=build.rs");
 }
