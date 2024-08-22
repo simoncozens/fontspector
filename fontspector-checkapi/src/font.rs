9912f84764563610bd22c7091d477c038b06d064
Simon Cozens <simon@simon-cozens.org>
2024-08-22 11:46:03 +0100

    Some useful functions

--- fontspector-checkapi/src/font.rs
+++ fontspector-checkapi/src/font.rs
@@ -4,10 +4,10 @@ use skrifa::{
     charmap::Charmap,
     font::FontRef,
     string::{LocalizedStrings, StringId},
-    MetadataProvider, Tag,
+    GlyphId, MetadataProvider, Tag,
 };
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     error::Error,
     path::{Path, PathBuf},
 };
@@ -15,7 +15,9 @@ use std::{
 pub struct TestFont<'a> {
     pub filename: PathBuf,
     font_data: &'a [u8],
+    // things it's worth caching
     _codepoints: HashSet<u32>,
+    _instance_coordinates: Vec<(String, HashMap<String, f32>)>,
 }
 
 pub const TTF: FileType = FileType { pattern: "*.ttf" };
@@ -36,10 +38,28 @@ impl TestFont<'_> {
     ) -> Result<TestFont<'a>, Box<dyn Error>> {
         let font = FontRef::new(font_data)?;
         let _codepoints = Charmap::new(&font).mappings().map(|(u, _gid)| u).collect();
+        let _axes = font.axes();
+        let _instance_coordinates = font
+            .named_instances()
+            .iter()
+            .map(|ni| {
+                let instance_name = font
+                    .localized_strings(ni.subfamily_name_id())
+                    .english_or_first()
+                    .map(|s| s.chars().collect::<String>())
+                    .unwrap_or("Unnamed".to_string());
+                let coords = ni
+                    .user_coords()
+                    .zip(font.axes().iter())
+                    .map(|(coord, axis)| (axis.tag().to_string(), coord));
+                (instance_name, coords.collect())
+            })
+            .collect();
         Ok(TestFont {
             filename: filename.to_path_buf(),
             font_data,
             _codepoints,
+            _instance_coordinates,
         })
     }
 
@@ -65,11 +85,37 @@ impl TestFont<'_> {
         self.font().localized_strings(name_id)
     }
 
+    pub fn glyph_name_for_id(&self, gid: GlyphId) -> String {
+        if let Ok(Some(name)) = self
+            .font()
+            .post()
+            .map(|post| post.glyph_name(gid).map(|x| x.to_string()))
+        {
+            name
+        } else {
+            format!("gid{:}", gid)
+        }
+    }
+
+    pub fn glyph_name_for_unicode(&self, u: impl Into<u32>) -> Option<String> {
+        self.font()
+            .charmap()
+            .map(u)
+            .map(|gid| self.glyph_name_for_id(gid))
+    }
+
     pub fn is_variable_font(&self) -> bool {
         self.has_table(b"fvar")
     }
 
     pub fn codepoints(&self) -> &HashSet<u32> {
         &self._codepoints
+    }
+
+    /// Returns an iterator over the named instances in the font.
+    ///
+    /// Each item is a tuple of the instance name and a map of axis tag to user coordinate value.
+    pub fn named_instances(&self) -> impl Iterator<Item = &(String, HashMap<String, f32>)> + '_ {
+        self._instance_coordinates.iter()
     }
 }
