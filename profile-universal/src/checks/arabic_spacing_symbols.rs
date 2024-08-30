fd0cbffdbd2cc883e873f07cc0fad2ed4a7b6ba7
Simon Cozens <simon@simon-cozens.org>
2024-08-30 14:17:28 +0100

    Run rustfmt

--- profile-universal/src/checks/arabic_spacing_symbols.rs
+++ profile-universal/src/checks/arabic_spacing_symbols.rs
@@ -1,23 +1,23 @@
 use fontspector_checkapi::{prelude::*, testfont, FileTypeConvert};
 
 const ARABIC_SPACING_SYMBOLS: [u16; 17] = [
-    0xFBB2,  // Dot Above
-    0xFBB3,  // Dot Below
-    0xFBB4,  // Two Dots Above
-    0xFBB5,  // Two Dots Below
-    0xFBB6,  // Three Dots Above
-    0xFBB7,  // Three Dots Below
-    0xFBB8,  // Three Dots Pointing Downwards Above
-    0xFBB9,  // Three Dots Pointing Downwards Below
-    0xFBBA,  // Four Dots Above
-    0xFBBB,  // Four Dots Below
-    0xFBBC,  // Double Vertical Bar Below
-    0xFBBD,  // Two Dots Vertically Above
-    0xFBBE,  // Two Dots Vertically Below
-    0xFBBF,  // Ring
-    0xFBC0,  // Small Tah Above
-    0xFBC1,  // Small Tah Below
-    0xFBC2,  // Wasla Above
+    0xFBB2, // Dot Above
+    0xFBB3, // Dot Below
+    0xFBB4, // Two Dots Above
+    0xFBB5, // Two Dots Below
+    0xFBB6, // Three Dots Above
+    0xFBB7, // Three Dots Below
+    0xFBB8, // Three Dots Pointing Downwards Above
+    0xFBB9, // Three Dots Pointing Downwards Below
+    0xFBBA, // Four Dots Above
+    0xFBBB, // Four Dots Below
+    0xFBBC, // Double Vertical Bar Below
+    0xFBBD, // Two Dots Vertically Above
+    0xFBBE, // Two Dots Vertically Below
+    0xFBBF, // Ring
+    0xFBC0, // Small Tah Above
+    0xFBC1, // Small Tah Below
+    0xFBC2, // Wasla Above
 ];
 
 #[check(
@@ -31,14 +31,16 @@ const ARABIC_SPACING_SYMBOLS: [u16; 17] = [
         for their original purpose as stand-alone symbols to used in pedagogical
         contexts discussing Arabic consonantal marks.
     ",
-    proposal = "https://github.com/googlefonts/fontbakery/issues/4295",
+    proposal = "https://github.com/googlefonts/fontbakery/issues/4295"
 )]
 fn arabic_spacing_symbols(t: &Testable, _context: &Context) -> CheckFnResult {
     let mut problems: Vec<Status> = vec![];
     let f = testfont!(t);
-    let cmap = f.get_cmap()
+    let cmap = f
+        .get_cmap()
         .map_err(|_| CheckError::Error("Font lacks a cmap table".to_string()))?;
-    let gdef = f.get_gdef()
+    let gdef = f
+        .get_gdef()
         .map_err(|_| CheckError::Error("Font lacks a gdef table".to_string()))?;
 
     let _class_def = match gdef.glyph_class_def() {
@@ -53,8 +55,13 @@ fn arabic_spacing_symbols(t: &Testable, _context: &Context) -> CheckFnResult {
         if gid.is_some()
         // && class_def.get(gid.ok_or("Failed to read gid")?) == 3
         {
-            problems.push(Status::fail("gdef-mark", &format!(
-                "U+{:04X} is defined in GDEF as a mark (class 3).", codepoint)));
+            problems.push(Status::fail(
+                "gdef-mark",
+                &format!(
+                    "U+{:04X} is defined in GDEF as a mark (class 3).",
+                    codepoint
+                ),
+            ));
         }
     }
 
