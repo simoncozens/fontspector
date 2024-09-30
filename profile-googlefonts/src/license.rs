67722089bbd8292071deb3bad694b3bc18ddcd39
Simon Cozens <simon@simon-cozens.org>
2024-09-30 14:03:53 +0100

    Prototype writing changed test fonts

--- profile-googlefonts/src/license.rs
+++ profile-googlefonts/src/license.rs
@@ -21,7 +21,7 @@ use skrifa::MetadataProvider;
 
         This check ensures \"Reserved Font Name\" is not mentioned in the name table.
     ",
-    proposal="https://github.com/fonttools/fontbakery/issues/1380",
+    proposal = "https://github.com/fonttools/fontbakery/issues/1380"
 )]
 fn name_rfn(t: &Testable, _context: &Context) -> CheckFnResult {
     let f = testfont!(t);
@@ -31,44 +31,54 @@ fn name_rfn(t: &Testable, _context: &Context) -> CheckFnResult {
     let str_data = f.font().name()?.string_data();
 
     for name in records {
-        let name_string = name.string(str_data)?
-                              .chars()
-                              .collect::<String>()
-                              .to_string();
-        if name_string.contains(
-            "This license is copied below, and is also available with a FAQ") {
+        let name_string = name
+            .string(str_data)?
+            .chars()
+            .collect::<String>()
+            .to_string();
+        if name_string.contains("This license is copied below, and is also available with a FAQ") {
             /* This is the OFL text in a name table entry.
-               It contains the term 'Reserved Font Name' in one of its clauses,
-               so we will ignore this here. */
+            It contains the term 'Reserved Font Name' in one of its clauses,
+            so we will ignore this here. */
             continue;
         }
 
-        let familyname = f.font()
-                          .localized_strings(NameId::FAMILY_NAME)
-                          .english_or_first()
-                          .ok_or(CheckError::Error("No name ID 1".to_string()))?
-                          .chars()
-                          .collect::<String>();
+        let familyname = f
+            .font()
+            .localized_strings(NameId::FAMILY_NAME)
+            .english_or_first()
+            .ok_or(CheckError::Error("No name ID 1".to_string()))?
+            .chars()
+            .collect::<String>();
+        #[allow(clippy::unwrap_used)]
         let re = Regex::new(r"with [Rr]eserved [Ff]ont [Nn]ame '(?<rfn>[^']*)'").unwrap();
         let matches = re.captures(&name_string);
 
         if matches.is_some() {
+            #[allow(clippy::expect_used)]
             let rfn = &matches.expect("wont happen")["rfn"];
             if familyname.contains(rfn) {
-                problems.push(Status::fail("rfn",
-                    &format!("Name table entry contains \"Reserved Font Name\":\n\
+                problems.push(Status::fail(
+                    "rfn",
+                    &format!(
+                        "Name table entry contains \"Reserved Font Name\":\n\
                               \t\"{}\"\n\
                               \n\
                               This is bad except in a few specific rare cases.",
-                              name_string)));
-            }
-            else {
-                problems.push(Status::warn("legacy-familyname",
-                    &format!("Name table entry contains \"Reserved Font Name\" for a \
+                        name_string
+                    ),
+                ));
+            } else {
+                problems.push(Status::warn(
+                    "legacy-familyname",
+                    &format!(
+                        "Name table entry contains \"Reserved Font Name\" for a \
                               family name ('{}') that differs \
                               from the currently used family name ('{}'), \
                               which is fine.",
-                              rfn, familyname)));
+                        rfn, familyname
+                    ),
+                ));
             }
         }
     }
@@ -76,19 +86,31 @@ fn name_rfn(t: &Testable, _context: &Context) -> CheckFnResult {
 }
 
 #[cfg(test)]
+#[allow(clippy::unwrap_used)]
 mod tests {
 
-// --- CODETESTING ---
-    use fontspector_checkapi::{Context, StatusCode, CheckResult};
+    use std::collections::BTreeSet;
+
+    // --- CODETESTING ---
+    use fontspector_checkapi::{CheckResult, Context, StatusCode};
     use serde_json::Map;
+    use write_fonts::{tables::name::NameRecord, FontBuilder};
 
-    macro_rules! TEST_FILE {($fname:expr) => (
-        Testable::new(
-            concat!(env!("CARGO_MANIFEST_DIR"), "/resources/test/", $fname)
-        ).unwrap()
-    )}
+    macro_rules! TEST_FILE {
+        ($fname:expr) => {
+            Testable::new(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/resources/test/",
+                $fname
+            ))
+            .unwrap()
+        };
+    }
 
-    fn run_check(check: fontspector_checkapi::Check<'_>, font: Testable) -> std::option::Option<CheckResult> {
+    fn run_check(
+        check: fontspector_checkapi::Check<'_>,
+        font: Testable,
+    ) -> std::option::Option<CheckResult> {
         let ctx: fontspector_checkapi::Context = Context {
             skip_network: false,
             network_timeout: Some(10),
@@ -103,85 +125,137 @@ mod tests {
         assert_eq!(status, StatusCode::Pass);
     }
 
-
-    fn assert_results_contain(check_result: std::option::Option<CheckResult>, severity: StatusCode, code: Option<String>) {
+    #[allow(dead_code)]
+    fn assert_results_contain(
+        check_result: std::option::Option<CheckResult>,
+        severity: StatusCode,
+        code: Option<String>,
+    ) {
         let subresults = check_result.unwrap().subresults;
-        assert!(subresults.iter().any(|subresult| subresult.severity == severity && subresult.code == code));
+        assert!(subresults
+            .iter()
+            .any(|subresult| subresult.severity == severity && subresult.code == code));
     }
 
-// --- end of CODETESTING ---
+    // --- end of CODETESTING ---
+
+    use crate::constants::OFL_BODY_TEXT;
+    use write_fonts::tables::name::Name;
 
     use super::*;
     //use crate::constants::OFL_BODY_TEXT;
 
     #[test]
-    fn pass_with_good_font(){
+    fn pass_with_good_font() {
         let font: Testable = TEST_FILE!("nunito/Nunito-Regular.ttf");
         assert_pass(run_check(super::name_rfn, font));
     }
 
     #[test]
-    fn pass_with_good_font_containing_ofl_full_text(){
+    fn pass_with_good_font_containing_ofl_full_text() {
         /* The OFL text contains the term 'Reserved Font Name',
-           which should not cause a FAIL: */
-        let font: Testable = TEST_FILE!("nunito/Nunito-Regular.ttf");
+        which should not cause a FAIL: */
+        let mut font: Testable = TEST_FILE!("nunito/Nunito-Regular.ttf");
 
-/* FIXME:
-        let f = testfont!(font);
-        f.font().name().setName(
-            OFL_BODY_TEXT,
+        let f = TTF.from_testable(&font).unwrap();
+        let name = f.font().name().unwrap();
+        let new_record = NameRecord::new(
+            3,
+            1,
+            0x0409,
             NameId::LICENSE_DESCRIPTION,
-            3,      // PlatformID.WINDOWS
-            1,      // WindowsEncodingID.UNICODE_BMP
-            0x0409, // WindowsLanguageID.ENGLISH_USA
+            OFL_BODY_TEXT.to_string().into(),
         );
-*/
+        let mut new_records: BTreeSet<NameRecord> = name
+            .name_record()
+            .iter()
+            .filter(|record| record.name_id() != NameId::LICENSE_DESCRIPTION)
+            .map(|r| {
+                #[allow(clippy::unwrap_used)]
+                NameRecord::new(
+                    r.platform_id(),
+                    r.encoding_id(),
+                    r.language_id(),
+                    r.name_id(),
+                    r.string(name.string_data())
+                        .unwrap()
+                        .chars()
+                        .collect::<String>()
+                        .to_string()
+                        .into(),
+                )
+            })
+            .collect();
+        new_records.insert(new_record);
+        let new_nametable = Name::new(new_records);
+        /* FIXME:
+                f.font().name().setName(
+                    OFL_BODY_TEXT,
+                    NameId::LICENSE_DESCRIPTION,
+                    3,      // PlatformID.WINDOWS
+                    1,      // WindowsEncodingID.UNICODE_BMP
+                    0x0409, // WindowsLanguageID.ENGLISH_USA
+                );
+        */
+        let mut new_font = FontBuilder::new();
+        new_font.add_table(&new_nametable).unwrap();
+        for table in f.font().table_directory.table_records() {
+            let tag = table.tag.get();
+            if tag == *b"name" {
+                continue;
+            } else if let Some(table) = f.font().table_data(tag) {
+                new_font.add_raw(tag, table);
+            }
+        }
+        let new_bytes = new_font.build();
+
+        font.contents = new_bytes;
         assert_pass(run_check(super::name_rfn, font));
     }
 }
 
 /* TODO:
 
-    # NOTE: This is not a real copyright statement. It is only meant to test the check.
-    with_nunito_rfn = (
-        "Copyright 2022 The Nunito Project Authors"
-        " (https://github.com/googlefonts/NunitoSans),"
-        " with Reserved Font Name Nunito."
-    )
-    ttFont["name"].setName(
-        with_nunito_rfn,
-        NameID.VERSION_STRING,
-        PlatformID.WINDOWS,
-        WindowsEncodingID.UNICODE_BMP,
-        WindowsLanguageID.ENGLISH_USA,
-    )
-    assert_results_contain(
-        check(ttFont),
-        FAIL,
-        "rfn",
-        'with "Reserved Font Name Nunito" on a name table entry...',
-    )
+# NOTE: This is not a real copyright statement. It is only meant to test the check.
+with_nunito_rfn = (
+    "Copyright 2022 The Nunito Project Authors"
+    " (https://github.com/googlefonts/NunitoSans),"
+    " with Reserved Font Name Nunito."
+)
+ttFont["name"].setName(
+    with_nunito_rfn,
+    NameID.VERSION_STRING,
+    PlatformID.WINDOWS,
+    WindowsEncodingID.UNICODE_BMP,
+    WindowsLanguageID.ENGLISH_USA,
+)
+assert_results_contain(
+    check(ttFont),
+    FAIL,
+    "rfn",
+    'with "Reserved Font Name Nunito" on a name table entry...',
+)
 
-    # NOTE: This is not a real copyright statement. It is only meant to test the check.
-    with_other_familyname_rfn = (
-        "Copyright 2022 The FooBar Project Authors"
-        " (https://github.com/foo/bar),"
-        " with Reserved Font Name FooBar."
-    )
-    ttFont["name"].setName(
-        with_other_familyname_rfn,
-        NameID.VERSION_STRING,
-        PlatformID.WINDOWS,
-        WindowsEncodingID.UNICODE_BMP,
-        WindowsLanguageID.ENGLISH_USA,
-    )
-    msg = assert_results_contain(
-        check(ttFont),
-        WARN,
-        "legacy-familyname",
-        'with "Reserved Font Name" that references an older'
-        " familyname not being used in this font project...",
-    )
-    assert "(FooBar)" in msg
+# NOTE: This is not a real copyright statement. It is only meant to test the check.
+with_other_familyname_rfn = (
+    "Copyright 2022 The FooBar Project Authors"
+    " (https://github.com/foo/bar),"
+    " with Reserved Font Name FooBar."
+)
+ttFont["name"].setName(
+    with_other_familyname_rfn,
+    NameID.VERSION_STRING,
+    PlatformID.WINDOWS,
+    WindowsEncodingID.UNICODE_BMP,
+    WindowsLanguageID.ENGLISH_USA,
+)
+msg = assert_results_contain(
+    check(ttFont),
+    WARN,
+    "legacy-familyname",
+    'with "Reserved Font Name" that references an older'
+    " familyname not being used in this font project...",
+)
+assert "(FooBar)" in msg
 
-    } */
+} */
