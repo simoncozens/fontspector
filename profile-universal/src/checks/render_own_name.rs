7343b74b890c3cd54d1d83a0b2e841a4b2b0a348
Simon Cozens <simon@simon-cozens.org>
2024-11-13 16:54:30 +0000

    Oops missing

---
+++ profile-universal/src/checks/render_own_name.rs
@@ -0,0 +1,33 @@
+use fontspector_checkapi::{prelude::*, testfont, FileTypeConvert};
+use read_fonts::tables::name::NameId;
+use skrifa::MetadataProvider;
+
+#[check(
+    id = "render_own_name",
+    title = "Ensure font can render its own name.",
+    rationale = "
+        A base expectation is that a font family's regular/default (400 roman) style
+        can render its 'menu name' (nameID 1) in itself.
+    ",
+    proposal = "https://github.com/fonttools/fontbakery/issues/3159"
+)]
+fn render_own_name(t: &Testable, _context: &Context) -> CheckFnResult {
+    let f = testfont!(t);
+    let name = f
+        .font()
+        .localized_strings(NameId::FAMILY_NAME)
+        .english_or_first()
+        .ok_or(CheckError::Error("Family name not found".to_string()))?;
+    let codepoints = f.codepoints();
+    if name.chars().any(|c| !codepoints.contains(&(c as u32))) {
+        Ok(Status::just_one_fail(
+            "render-own-name",
+            &format!(
+                ".notdef glyphs were found when attempting to render {}",
+                name.chars().collect::<String>()
+            ),
+        ))
+    } else {
+        Ok(Status::just_one_pass())
+    }
+}
