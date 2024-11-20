0e99283a32aef8131111bd704b8d8515a956ea6d
Simon Cozens <simon@simon-cozens.org>
2024-11-20 22:40:09 +0000

    Update test

--- fontspector-web/test.js
+++ fontspector-web/test.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 // For this to work you need to compile with wasm-pack --target nodejs!
 const fontspector = require('./pkg');
 const data = fs.readFileSync('../Nunito[wght].ttf')
-console.log(fontspector.check_fonts({"Nunito[wght].ttf": data}))
+console.log(fontspector.check_fonts({"Nunito[wght].ttf": data}, "universal"))
