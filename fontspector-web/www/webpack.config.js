b2b44575174b187269303f363dbc75690c244879
Simon Cozens <simon@simon-cozens.org>
2024-11-22 06:23:06 +0000

    Upgrade webpack

--- fontspector-web/www/webpack.config.js
+++ fontspector-web/www/webpack.config.js
@@ -1,6 +1,7 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 const crypto = require("crypto");
+const { experiments } = require("webpack");
 const crypto_orig_createHash = crypto.createHash;
 crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
@@ -10,11 +11,13 @@ module.exports = {
     path: path.resolve(__dirname, "..", "..", "docs"),
     filename: "bootstrap.js"
   },
+  experiments: {
+    asyncWebAssembly: true
+  },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin([
         'index.html',
-        'AND-Regular.ttf',
         '*.css',
         '*.png',
         'cmark-gfm.js',
