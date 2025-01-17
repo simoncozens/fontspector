c040fe032d87f2db4f2546f2b9ae53fc524c4181
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2025-01-17 03:33:49 -0300

    googlefonts/varfont/bold_wght_coord => varfont/bold_wght_coord
    (https://github.com/fonttools/fontbakery/pull/4938)

--- fontspector-py/tests/test_checks_universal.py
+++ fontspector-py/tests/test_checks_universal.py
@@ -958,3 +958,27 @@ def test_check_gsub_smallcaps_before_ligatures(check):
     smcp_feature.LookupListIndex = [1]
     liga_feature.LookupListIndex = [0]
     assert_results_contain(check(ttFont), FAIL, "feature-ordering")
+
+
+@pytest.mark.skip("Check not ported yet.")
+@check_id("varfont/bold_wght_coord")
+def test_check_varfont_bold_wght_coord(check):
+    """The variable font 'wght' (Weight) axis coordinate
+    must be 700 on the 'Bold' instance."""
+
+    # Our reference varfont CabinVFBeta.ttf
+    # has a good Bold:wght coordinate
+    ttFont = TTFont("data/test/cabinvfbeta/CabinVFBeta.ttf")
+    assert_PASS(check(ttFont), "with a good Bold:wght coordinate...")
+
+    # We then change the value to ensure the problem is properly detected by the check:
+    ttFont["fvar"].instances[3].coordinates["wght"] = 600
+    assert_results_contain(
+        check(ttFont), FAIL, "wght-not-700", "with a bad Bold:wght coordinage (600)..."
+    )
+
+    # Check we skip when we don't have a 700 weight.
+    ttFont = TTFont("data/test/cabinvfbeta/CabinVFBeta.ttf")
+    del ttFont["fvar"].instances[3]
+    ttFont["fvar"].axes[0].maxValue = 600
+    assert_results_contain(check(ttFont), SKIP, "no-bold-weight")
