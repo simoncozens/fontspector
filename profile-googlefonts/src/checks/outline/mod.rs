9b38aa50dc9b1323d2ea8fbb570895e4a301e46a
Simon Cozens <simon@simon-cozens.org>
2025-01-22 16:26:34 +0000

    Make a generic close_but_not_on

--- profile-googlefonts/src/checks/outline/mod.rs
+++ profile-googlefonts/src/checks/outline/mod.rs
@@ -1,4 +1,6 @@
 mod alignment_miss;
+use std::ops::Sub;
+
 pub use alignment_miss::alignment_miss;
 mod direction;
 pub use direction::direction;
@@ -6,3 +8,10 @@ mod jaggy_segments;
 pub use jaggy_segments::jaggy_segments;
 // mod semi_vertical;
 // mod short_segments;
+
+pub(crate) fn close_but_not_on<T>(expected: T, actual: T, epsilon: T) -> bool
+where
+    T: Sub<Output = T> + PartialOrd + Copy + num_traits::sign::Signed,
+{
+    (actual - expected).abs() <= epsilon && actual != expected
+}
