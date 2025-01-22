79bf024800e14c0b4532258863469b6af89bd7f8
Simon Cozens <simon@simon-cozens.org>
2025-01-22 10:12:12 +0000

    Rename check module

--- profile-googlefonts/src/checks/mod.rs
+++ profile-googlefonts/src/checks/mod.rs
@@ -1,7 +1,7 @@
 pub mod axes_match;
 pub mod color_fonts;
 pub mod fstype;
-pub mod googlefonts_weightclass;
 pub mod name_description_max_length;
 pub mod render_own_name;
 pub mod tofu;
+pub mod weightclass;
