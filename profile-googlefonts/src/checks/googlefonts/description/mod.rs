6e24d8e03b4a315cf3bad7c430a7e285a2b37b08
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2025-01-29 14:11:53 -0300

    sort checks/googlefonts/description/mod.rs

--- profile-googlefonts/src/checks/googlefonts/description/mod.rs
+++ profile-googlefonts/src/checks/googlefonts/description/mod.rs
@@ -1,16 +1,17 @@
-mod eof_linebreak;
-mod min_length;
-pub use eof_linebreak::eof_linebreak;
-pub use min_length::min_length;
 mod broken_links;
-pub use broken_links::broken_links;
+mod eof_linebreak;
 mod git_url;
-pub use git_url::git_url;
-mod has_unsupported_elements;
-pub use has_unsupported_elements::has_unsupported_elements;
 mod has_article;
-pub use has_article::has_article;
+mod has_unsupported_elements;
+mod min_length;
 mod urls;
-pub use urls::urls;
 mod valid_html;
+
+pub use broken_links::broken_links;
+pub use eof_linebreak::eof_linebreak;
+pub use git_url::git_url;
+pub use has_article::has_article;
+pub use has_unsupported_elements::has_unsupported_elements;
+pub use min_length::min_length;
+pub use urls::urls;
 pub use valid_html::valid_html;
